259b8f883c0fcc01c8c25a28db0759e16f92e9a3
ASUS <ASUS@LAPTOP-53HUPL4C>
2018-09-14 18:19:45 +0700

    Tambah Input

--- src/jos7/FibonacciDynamic.java
+++ src/jos7/FibonacciDynamic.java
@@ -9,8 +9,15 @@ public class FibonacciDynamic {
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
-    }   
+    } 
+    private static int tampilInput() {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = scanner.nextInt();
+        
+        return n;
     
    }
-  
+} 
     
