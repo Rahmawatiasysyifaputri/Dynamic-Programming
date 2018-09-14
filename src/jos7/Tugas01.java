e50b59f45733feeb345664a0002c9d77539ca2cb
ASUS <ASUS@LAPTOP-53HUPL4C>
2018-09-14 20:27:06 +0700

    Deklarasi

---
+++ src/jos7/Tugas01.java
@@ -0,0 +1,19 @@
+package jos7;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Tugas01 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int uang = input.nextInt();
+        int a = 0, b = 0, c = 0, d = 0, e = 0;
+        int[] n = new int [] {100, 500, 1000, 2000, 5000};
+        System.out.print("Jumlah Uang? Rp");
+        
+        
+        
+    }
+    
+}
