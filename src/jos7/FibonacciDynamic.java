bda12bbd5ad87be1db78c763fe5edc74d7cf01e8
ASUS <ASUS@LAPTOP-53HUPL4C>
2018-09-14 18:22:18 +0700

    Tambah Hitung Fibonacci

--- src/jos7/FibonacciDynamic.java
+++ src/jos7/FibonacciDynamic.java
@@ -19,5 +19,19 @@ public class FibonacciDynamic {
         return n;
     
    }
-} 
+     private static BigInteger fibo(int n) {
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++) {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
+     }
+}   
+
     
